1158e607385dd034fce4fb7ac6ca4c701ea41737
이경용 <sleekydz86@icloud.com>
2024-02-07 12:27:19 +0900

    기본페이지 셋팅

--- src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
+++ src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
@@ -1,4 +1,14 @@
 package com.vuestudy.vuesampleing.vueStudy;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/vueMain")
+    public String showVueMain(){
+        return "vueStudy/main";
+    }
 }
