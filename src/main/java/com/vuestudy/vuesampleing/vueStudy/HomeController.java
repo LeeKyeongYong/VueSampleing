b7369777bedaab8ff5630bba5f155c3019ce0d77
이경용 <sleekydz86@icloud.com>
2024-02-07 12:35:33 +0900

    타임리프를 이용한 상자 2개 생성, HTML, CSS 기초작업

--- src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
+++ src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
@@ -2,13 +2,23 @@ package com.vuestudy.vuesampleing.vueStudy;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private int count1 = 0;
+    private int count2 = 0;
+
     @GetMapping("/vueMain")
-    public String showVueMain(){
+    public String showVueMain(Model model){
+        count1++;
+        count2++;
+
+        model.addAttribute("count1",count1);
+        model.addAttribute("count2",count2);
+
         return "vueStudy/main";
     }
 }
