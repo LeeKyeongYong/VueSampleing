a4e04e43687a797a343b7ab7ed1352157bdf1af6
이경용 <sleekydz86@icloud.com>
2024-02-07 12:53:00 +0900

    ajax 작업을 위한 엔트포인트 생성 설명

--- src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
+++ src/main/java/com/vuestudy/vuesampleing/vueStudy/HomeController.java
@@ -4,6 +4,7 @@ package com.vuestudy.vuesampleing.vueStudy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -13,12 +14,34 @@ public class HomeController {
 
     @GetMapping("/vueMain")
     public String showVueMain(Model model){
-        count1++;
-        count2++;
+
 
         model.addAttribute("count1",count1);
         model.addAttribute("count2",count2);
 
         return "vueStudy/main";
+    }
+    @GetMapping("/count1")
+    @ResponseBody
+    public RsData<Integer> getCount1(Model model){
+        return new RsData<>("S-1","current count1",count1);
+    }
+
+    @GetMapping("/count2")
+    @ResponseBody
+    public RsData<Integer> getCount2(Model model){
+        return new RsData("S-1","current count2",count2);
+    }
+
+    @GetMapping("/increaseCount1")
+    @ResponseBody
+    public RsData<Integer> incraseCount1(Model model){
+        return new RsData<>("S-1","count1 increased",++count1);
+    }
+
+    @GetMapping("/increaseCount2")
+    @ResponseBody
+    public RsData<Integer> increaseCount2(Model model){
+        return new RsData<>("S-1","count2 increased",++count2);
     }
 }
--- src/main/java/com/vuestudy/vuesampleing/vueStudy/RsData.java
+++ src/main/java/com/vuestudy/vuesampleing/vueStudy/RsData.java
@@ -1,4 +1,12 @@
 package com.vuestudy.vuesampleing.vueStudy;
 
-public class RsData {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RsData<T> {
+    private String resultCode;
+    private String me;
+    private T data;
 }
